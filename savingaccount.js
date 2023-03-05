11d52476ec530b69e9e650d1aaaa5393b00c7f87
Matome <matome.sethaba@siemens.com>
2023-03-05 13:27:49 +0200

    open account with not less than 1000+ deposit

---
+++ savingaccount.js
@@ -0,0 +1,17 @@
+
+class SavingsAccount {
+    constructor() {
+      this.balance = 0;
+      this.transactionHistory = [];
+    }
+    openAccount(amount) {
+        if (amount >= 1000) {
+          this.balance = amount;
+          this.transactionHistory.push(`Account opened with a deposit of R${amount.toFixed(2)}`);
+          console.log("Your Account is successfully Opened.");
+          console.log(this.transactionHistory[0]);
+        } else {
+          console.log("Account opening failed. The minimum deposit is R1,000.00.");
+        }
+      }
+    }

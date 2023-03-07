911ce345e39119a9c9df7b4727842aa2332e14f7
Matome <matome.sethaba@siemens.com>
2023-03-07 10:14:37 +0200

    current account withdrwal function

--- currentaccount.js
+++ currentaccount.js
@@ -8,4 +8,13 @@ let currentAccount = {
     if (amount > (currentAccount.balance + currentAccount.overdraftLimit)) {
       console.log('Withdrawal amount exceeds account limit.');
       return;
+    }
+    function withdraw(amount) {
+      if (amount > (currentAccount.balance + currentAccount.overdraftLimit)) {
+        console.log('Withdrawal amount exceeds account limit.');
+        return;
+      }
+      currentAccount.balance -= amount;
+      currentAccount.transactionHistory.push(`Withdrawal of ${amount}`);
+      console.log(`Withdrawal of R${amount} successful. Current balance: R${currentAccount.balance}`);
     }}
